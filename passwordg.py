a4083c1b12d83b846b313f044e2b8cbd863cd161
rannatr <79092535+rannatr@users.noreply.github.com>
2021-02-15 16:14:28 +0300

    Add files via upload

---
+++ passwordg.py
@@ -0,0 +1,67 @@
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = " "
+
+for k in range(nr_letters):
+    for m in letters[k]:
+        m = random.choice(letters)
+        password = password + m
+
+for l in range(nr_symbols):
+    for t in symbols[l]:
+        t = random.choice(symbols)
+        password = password + t
+
+for j in range(nr_numbers):
+    for x in numbers[j]:
+        x = random.choice(numbers)
+        password = password + x
+print(password)
+
+# HARD LEVEL:ORDER RANDOMİSED
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+plist = []
+
+for k in range(nr_letters):
+    for m in letters[k]:
+        plist += random.choice(letters)
+
+for l in range(nr_symbols):
+    for t in symbols[l]:
+        plist += random.choice(symbols)
+
+for j in range(nr_numbers):
+    for x in numbers[j]:
+        plist += random.choice(numbers)
+
+random.shuffle(plist)
+for k in range(len(plist)):
+    print(plist[k], end="")
+
